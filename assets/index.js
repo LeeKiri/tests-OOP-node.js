3779d72cc9dec339febaba6584c1a908d240b7bb
Lee Kiri <leekiritempleton@gmail.com>
2021-01-27 13:45:44 +1100

    initial file structure and HTML set up

---
+++ assets/index.js
@@ -0,0 +1,6 @@
+const employee = require("./lib/employee");
+const engineer = require("./lib/engineer");
+const manager = require("./lib/manager");
+const intern = require("./lib/intern");
+const inquirer = require("inquirer");
+const fs = require("fs");
